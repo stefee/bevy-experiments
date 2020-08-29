578757a267eac86a6630282e8756eba444122663
Stef <stef.jones@condenast.com>
2020-08-29 18:32:39 +0100

    progress

--- bevy_ui_storybook/src/main.rs
+++ bevy_ui_storybook/src/main.rs
@@ -1,5 +1,7 @@
 use bevy::prelude::*;
 
+const DEBUG_MATERIALS: bool = false;
+
 fn main() {
     App::build()
         .add_resource(bevy::window::WindowDescriptor {
@@ -14,11 +16,15 @@ fn main() {
         .run();
 }
 
-fn setup(
-    mut commands: Commands,
-    // asset_server: Res<AssetServer>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-) {
+fn debug_color(materials: &mut ResMut<Assets<ColorMaterial>>, color: ColorMaterial) -> Handle<ColorMaterial> {
+    if DEBUG_MATERIALS {
+        materials.add(color)
+    } else {
+        Default::default()
+    }
+}
+
+fn setup(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
     commands
         // ui camera
         .spawn(UiCameraComponents::default())
@@ -28,26 +34,133 @@ fn setup(
                 align_items: AlignItems::Stretch,
                 ..Default::default()
             },
-            material: materials.add(Color::rgb(1.0, 0.0, 1.0).into()),
+            material: debug_color(&mut materials, Color::rgb(1.0, 0.0, 1.0).into()),
             ..Default::default()
         })
         .with_children(|parent| {
             parent
+                //
+                // sidebar
+                //
                 .spawn(NodeComponents {
                     style: Style {
                         flex_basis: Val::Percent(20.0),
+                        flex_direction: FlexDirection::ColumnReverse,
                         ..Default::default()
                     },
-                    material: materials.add(Color::rgb(1.0, 0.0, 0.0).into()),
+                    material: debug_color(&mut materials, Color::rgb(1.0, 0.0, 0.0).into()),
                     ..Default::default()
                 })
+                .with_children(|parent| {
+                    parent
+                        .spawn(NodeComponents {
+                            style: Style {
+                                flex_basis: Val::Px(100.0),
+                                ..Default::default()
+                            },
+                            material: debug_color(&mut materials, Color::rgb(0.2, 0.2, 1.0).into()),
+                            ..Default::default()
+                        })
+                        .spawn(border_horizontal_node(&mut materials))
+                        .spawn(NodeComponents {
+                            style: Style {
+                                flex_basis: Val::Px(100.0),
+                                ..Default::default()
+                            },
+                            material: debug_color(&mut materials, Color::rgb(0.3, 0.3, 1.0).into()),
+                            ..Default::default()
+                        })
+                        .spawn(border_horizontal_node(&mut materials))
+                        .spawn(NodeComponents {
+                            style: Style {
+                                flex_basis: Val::Px(100.0),
+                                ..Default::default()
+                            },
+                            material: debug_color(&mut materials, Color::rgb(0.4, 0.4, 1.0).into()),
+                            ..Default::default()
+                        })
+                        .spawn(border_horizontal_node(&mut materials))
+                        .spawn(NodeComponents {
+                            style: Style {
+                                flex_basis: Val::Px(100.0),
+                                ..Default::default()
+                            },
+                            material: debug_color(&mut materials, Color::rgb(0.5, 0.5, 1.0).into()),
+                            ..Default::default()
+                        })
+                        .spawn(border_horizontal_node(&mut materials))
+                        .spawn(NodeComponents {
+                            style: Style {
+                                flex_basis: Val::Px(100.0),
+                                ..Default::default()
+                            },
+                            material: debug_color(&mut materials, Color::rgb(0.6, 0.6, 1.0).into()),
+                            ..Default::default()
+                        })
+                        .spawn(border_horizontal_node(&mut materials))
+                        .spawn(NodeComponents {
+                            style: Style {
+                                flex_basis: Val::Px(100.0),
+                                ..Default::default()
+                            },
+                            material: debug_color(&mut materials, Color::rgb(0.7, 0.7, 1.0).into()),
+                            ..Default::default()
+                        })
+                        .spawn(border_horizontal_node(&mut materials))
+                        .spawn(NodeComponents {
+                            style: Style {
+                                flex_basis: Val::Px(100.0),
+                                ..Default::default()
+                            },
+                            material: debug_color(&mut materials, Color::rgb(0.8, 0.8, 1.0).into()),
+                            ..Default::default()
+                        })
+                        .spawn(border_horizontal_node(&mut materials))
+                        .spawn(NodeComponents {
+                            style: Style {
+                                flex_basis: Val::Px(100.0),
+                                ..Default::default()
+                            },
+                            material: debug_color(&mut materials, Color::rgb(0.9, 0.9, 1.0).into()),
+                            ..Default::default()
+                        });
+                })
+                //
+                // vertical border
+                //
+                .spawn(border_vertical_node(&mut materials))
+                //
+                // main
+                //
                 .spawn(NodeComponents {
                     style: Style {
                         flex_basis: Val::Percent(80.0),
                         ..Default::default()
                     },
-                    material: materials.add(Color::rgb(0.0, 1.0, 0.0).into()),
+                    material: debug_color(&mut materials, Color::rgb(0.0, 1.0, 0.0).into()),
                     ..Default::default()
                 });
         });
+}
+
+fn border_vertical_node(materials: &mut ResMut<Assets<ColorMaterial>>) -> NodeComponents {
+    NodeComponents {
+        style: Style {
+            size: Size::new(Val::Px(4.0), Val::Percent(100.0)),
+            ..Default::default()
+        },
+        material: materials.add(Color::rgb(0.8, 0.8, 0.8).into()),
+        ..Default::default()
+    }
+}
+
+fn border_horizontal_node(materials: &mut ResMut<Assets<ColorMaterial>>) -> NodeComponents {
+    NodeComponents {
+        style: Style {
+            size: Size::new(Val::Percent(100.0), Val::Px(4.0)),
+            ..Default::default()
+        },
+        material: materials.add(Color::rgb(0.8, 0.8, 0.8).into()),
+        ..Default::default()
+    }
 }
