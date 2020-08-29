ae497ba39cfdddd8cdb7db1652eb701bc8eec2e5
Stef <stef.jones@condenast.com>
2020-08-29 13:33:45 +0100

    initial commit

---
+++ proj1/src/main.rs
@@ -0,0 +1,53 @@
+use bevy::prelude::*;
+
+fn main() {
+    App::build()
+        .add_resource(bevy::window::WindowDescriptor {
+            width: 1920,
+            height: 1080,
+            title: "Bevy UI Storybook Prototype".to_string(),
+            ..Default::default()
+        })
+        .add_resource(bevy::render::pass::ClearColor(Color::rgb(1.0, 1.0, 1.0)))
+        .add_default_plugins()
+        .add_startup_system(setup.system())
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    // asset_server: Res<AssetServer>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands
+        // ui camera
+        .spawn(UiCameraComponents::default())
+        .spawn(NodeComponents {
+            style: Style {
+                size: Size::new(Val::Px(200.0), Val::Px(200.0)),
+                align_items: AlignItems::Stretch,
+                ..Default::default()
+            },
+            material: materials.add(Color::rgb(1.0, 0.0, 1.0).into()),
+            ..Default::default()
+        })
+        .with_children(|parent| {
+            parent
+                .spawn(NodeComponents {
+                    style: Style {
+                        flex_basis: Val::Percent(50.0),
+                        ..Default::default()
+                    },
+                    material: materials.add(Color::rgb(1.0, 0.0, 0.0).into()),
+                    ..Default::default()
+                })
+                .spawn(NodeComponents {
+                    style: Style {
+                        flex_basis: Val::Percent(50.0),
+                        ..Default::default()
+                    },
+                    material: materials.add(Color::rgb(0.0, 1.0, 0.0).into()),
+                    ..Default::default()
+                });
+        });
+}
